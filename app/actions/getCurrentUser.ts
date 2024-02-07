f14d2371274de545fc7e0076eb801bc33926520e
talibd <talibkhan111105@gmail.com>
2024-02-07 16:27:25 +0530

    fix

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -27,8 +27,8 @@ export default async function getCurrentUser() {
 
     return {
       ...currentUser,
-      createdAt: currentUser.createdAt.toISOString(),
-      updatedAt: currentUser.updatedAt.toISOString(),
+      createdAt: currentUser.createAt.toISOString(),
+      updatedAt: currentUser.updateAt.toISOString(),
       emailVerified: 
         currentUser.emailVerified?.toISOString() || null,
     };
