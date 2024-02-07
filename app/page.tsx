4964f4d3a4584b726078bb73e344da64ca3c42be
talibd <talibkhan111105@gmail.com>
2024-02-07 13:46:38 +0530

    fix error

--- app/page.tsx
+++ app/page.tsx
@@ -1,56 +1,54 @@
-import getCurrentUser from "./actions/getCurrentUser";
-import getListings, { IListingParams } from "./actions/getListing";
+import Container from "@/app/components/Container";
+import ListingCard from "@/app/components/listing/ListingCard";
+import EmptyState from "@/app/components/EmptyState";
+
+import getListings, {IListingsParams} from "@/app/actions/getListing";
+import getCurrentUser from "@/app/actions/getCurrentUser";
 import ClientOnly from "./components/ClientOnly";
-import Container from "./components/Container";
-import EmptyState from "./components/EmptyState";
-import ListingCard from "./components/listing/ListingCard";
 
 interface HomeProps {
-  searchParams: IListingParams
-}
+  searchParams: IListingsParams
+};
 
 const Home = async ({ searchParams }: HomeProps) => {
-
   const listings = await getListings(searchParams);
   const currentUser = await getCurrentUser();
-
 
   if (listings.length === 0) {
     return (
       <ClientOnly>
         <EmptyState showReset />
       </ClientOnly>
-    )
+    );
   }
 
   return (
-   <ClientOnly>
-    <Container>
-      <div className="
-      pt-24
-      grid
-      grid-cols-1
-      sm:grid-cols-2
-      md:grid-cols-3
-      lg:grid-cols-4
-      xl:grid-cols-5
-      2xl:grid-cols-6
-      gap-8
-      ">
-        {listings.map((listing) => {
-          return (
-            <ListingCard 
-            key={listing.id}
-            data={listing}
-            currentUser={currentUser}
+    <ClientOnly>
+      <Container>
+        <div 
+          className="
+            pt-24
+            grid 
+            grid-cols-1 
+            sm:grid-cols-2 
+            md:grid-cols-3 
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-6
+            gap-8
+          "
+        >
+          {listings.map((listing: any) => (
+            <ListingCard
+              currentUser={currentUser}
+              key={listing.id}
+              data={listing}
             />
-          )
-        })}
-      </div>
-    </Container>
-   </ClientOnly>
-  );
+          ))}
+        </div>
+      </Container>
+    </ClientOnly>
+  )
 }
-
 
 export default Home;
